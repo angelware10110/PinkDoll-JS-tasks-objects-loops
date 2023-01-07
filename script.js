ba8412d97b3dbb558417227be7a697dda2f5e4f8
vytenisstaugaitis <30520456+vytenisstaugaitis@users.noreply.github.com>
2023-01-07 16:02:36 +0200

    init commit fix, lezzz go

--- script.js
+++ script.js
@@ -1,47 +1,71 @@
 //1.creating a wallet
 
-const pinigine = 0;
-const pinigai = 0;
+const pinigine = [];
 
 const pinigineCount = (index) => {
-    const pinigine = [];
-    let pinigai;
-    for (let i = 0; i <= index; i++){
-        pinigai = Math.trunc(Math.random()*10) + 1;
-      
-      pinigine[i] = pinigai;
-    }
-    console.table(pinigine);
+  let pinigai = 0;
+
+  for (let i = 0; i <= index - 1; i++){
+    pinigai = Math.trunc(Math.random()*10) + 1;
+    pinigine[i] = pinigai;
   }
-  pinigineCount(10);
+
+  console.table(pinigine);
+}
   
+pinigineCount(10);
+
+  
+
 //2.counting sum of my wallet
 
- let pinigaiSum = [1,2,3,4,5,6,7,8,9,10].reduceRight(function(a,b){return a+b;});
-  console.log(pinigaiSum);
+const pinigineSum = () => {
+  let pinigaiSum = 0;
+
+  pinigine.forEach(pinigai => {
+    pinigaiSum += pinigai;
+  });
+
+  console.log('2. counting sum: '+pinigaiSum);
+}
+
+pinigineSum();
+
 
 
 //3.counting paper $$
 
 const paperSumCount = () => {
-    for (let i = 0; i <= pinigine.length; i++) { 
-      if (pinigai > 2) { 
-      pinigine.reduce((a, b) => a + b);} 
+  let paperSum = 0;
+
+  for (let i = 0; i <= pinigine.length; i++) { 
+    if (pinigine[i] > 2) { 
+      paperSum += pinigine[i];
     }
-    console.log(pinigine);
-    console.log('pinigine');
   }
-  paperSumCount();
+
+  console.log('3. counting paper: '+paperSum);
+}
+
+paperSumCount();
+
+
 
 //4. spend small cash from a wallet
 
 const metalSumCount = () => {
-  for (let i = 0; i = <= pinigine.length; i++) {
-    if (pinigai < 2) {
-      pinigine(i) = 0 
+
+  for (let i = 0; i <= pinigine.length; i++) {
+    if (pinigine[i] <= 2) { 
+      pinigine[i] = 0;
     }
   }
+
+  console.log('4. spend small cash from a wallet: '+pinigine);
 }
+
+metalSumCount();
+
 
 //for loop suki pinigine ; if pinigas<2 ; tada pinigas = 0; pinigine(i) = 0 remove?is array elementa; how to remove value is array
 
